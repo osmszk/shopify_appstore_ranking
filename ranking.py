4f3b1d4f28134195268b892062aeda2ece45aca5
osuzuki <osuzuki@plegineer.com>
2020-09-15 09:52:29 +0900

    Fix

--- ranking.py
+++ ranking.py
@@ -15,19 +15,25 @@ html = driver.page_source.encode('utf-8')
 
 soup = BeautifulSoup(html, "html.parser")
 
+result = ''
 for i, content in enumerate(soup.find("div", {"id": "SearchResultsListings"}).find_all("div", {"class": "grid__item"})):
   
-  app_title = content.find("h4", {"class": "ui-app-card__name"}).text
-  discription = content.find("p", {"class": "ui-app-card__details"}).text
+  app_title = content.find("h4", {"class": "ui-app-card__name"}).text.replace(',', " ")
+  discription = content.find("p", {"class": "ui-app-card__details"}).text.replace(',', " ")
   review_text = content.find("div", {"class": "ui-star-rating__text"}).text #3.3 of 5 stars(646reviews)
   review_rate = review_text[:3]
   review_count = review_text.split('(')[1].split('reviews')[0] #replace('reviews)', '')
-  developer = content.find("div", {"class": "ui-app-card__developer-name"}).text[3:]
+  developer = content.find("div", {"class": "ui-app-card__developer-name"}).text[3:].replace(',', " ")
   print("apptitle:", app_title)
   print("discription:", discription)
   print("review:", review_rate, review_count)
   print("developer:", developer)
+  result = result + "{0},{1},{2},{3},{4}".format(app_title,discription,developer,review_rate,review_count)
+  result = result + '\n'
   print("-----------")
 
 driver.close()
 driver.quit()
+
+with open("output.csv", "w") as text_file:
+    print(result, file=text_file)
