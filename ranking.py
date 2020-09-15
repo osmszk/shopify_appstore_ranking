f8e33780012f14376fd7cf757c14b76f631b286d
osuzuki <osuzuki@plegineer.com>
2020-09-15 09:39:38 +0900

    Fix

--- ranking.py
+++ ranking.py
@@ -15,49 +15,19 @@ html = driver.page_source.encode('utf-8')
 
 soup = BeautifulSoup(html, "html.parser")
 
-# for i, content in enumerate(soup.select_one("#dismissable").find_all("ytd-video-renderer")):
-
-# soup.find("div", {"id": "SearchResultsListings"})
-# searchResultsListings = soup.select_one("#SearchResultsListings")
-# for (i,content) in enumerate(searchResultsListings.find_all("grid__item")):
-#   print(content.text)
-# div1 = soup.select_one("#SearchResultsListings > div.grid__item")
-# print(div1.text)
-print("*****")
-print(soup.find("div", {"id": "SearchResultsListings"}).find_all("div", {"class": "grid__item"})[0].text )
-print("---------")
 for i, content in enumerate(soup.find("div", {"id": "SearchResultsListings"}).find_all("div", {"class": "grid__item"})):
   
   app_title = content.find("h4", {"class": "ui-app-card__name"}).text
   discription = content.find("p", {"class": "ui-app-card__details"}).text
-  review_rate = content.find("div", {"class": "ui-star-rating__text"}).text
+  review_text = content.find("div", {"class": "ui-star-rating__text"}).text #3.3 of 5 stars(646reviews)
+  review_rate = review_text[:3]
+  review_count = review_text.split('(')[1].split('reviews')[0] #replace('reviews)', '')
   developer = content.find("div", {"class": "ui-app-card__developer-name"}).text[3:]
   print("apptitle:", app_title)
   print("discription:", discription)
-  print("review_rate:", review_rate)
+  print("review:", review_rate, review_count)
   print("developer:", developer)
   print("-----------")
-  
-  # /html/body/div[5]/main/section[2]/div/div[2]/div[2]/div[1]
-  # //*[@id="SearchResultsListings"]/div[1]
-
-# for i, content in enumerate(soup.select_one("#dismissable").find_all("ytd-video-renderer")):
-#   video_title = content.select_one("#video-title").get_text().strip()
-#   channel_name = content.select_one("#text > a").get_text().strip()
-#   play_count = content.select_one("#metadata-line > span:nth-child(1)").get_text().strip()
-#   post_date = content.select_one("#metadata-line > span:nth-child(2)").get_text().strip()
-#   image_url = content.select_one("#img").get('src')
-#   video_path = content.select_one("#video-title").get('href')
-
-#   print('%d位' % (i+1))
-#   print(video_title)
-#   print(channel_name)
-#   print(play_count)
-#   print(post_date)
-#   print(image_url)
-#   print(YOUTUBE_URL+video_path)
-#   print("------------------")
-
 
 driver.close()
 driver.quit()
